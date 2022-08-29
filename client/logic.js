93f9b0b72f36f7bc5cb201977eb2ef75881c34ea
Hannane <hannanekabuli@gmail.com>
2022-08-29 13:02:48 +0100

    first commit

---
+++ client/logic.js
@@ -0,0 +1,64 @@
+async function onLoad() {
+    await getProducts()
+ 
+}
+
+const getProducts = async (event) => {
+
+    try {
+        
+        const response = await fetch("http://localhost:3000/products")
+        const data = await response.json()
+        console.log(data)
+
+        for (let i = 0; i < data.length; i++) {
+            const product = data[i]
+            console.log(product.bookName)
+
+        const container = document.getElementById("books")
+        let productContainer = document.createElement("div")
+        productContainer.classList.add("productDiv")
+        let title = document.createElement("a")
+        title.innerHTML = product.bookName + product.author
+        container.append(productContainer)
+        productContainer.append(title) 
+        
+        }
+
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+const addProducts = async (event) => {
+    
+    try {
+        
+        const newProduct = {
+            bookName: "Harry Potter",
+            author: "(J: K. Rowling)"
+        }
+
+        
+        const response = await fetch("http://localhost:3000/products", {
+            method: "POST", 
+            headers: { "Content-Type": "application/json"}, 
+            body: JSON.stringify(newProduct)
+
+        }) 
+        
+        const data = await response.json()
+            console.log(data)
+
+        } catch(err) {
+            console.error(err)
+        }
+    }
+
+    
+document.getElementById("createBtn").addEventListener("click", getProducts)
+document.getElementById("collectBtn").addEventListener("click", addProducts)
+window.addEventListener('load', onLoad) 
+
+
+    
---
+++ index.js
@@ -0,0 +1,111 @@
+import express from "express";
+import axios from "axios";
+import { nanoid } from "nanoid";
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use("/", express.static("client"))
+
+
+
+app.get("/api/book", async (req, res) => {
+
+  try {
+
+    const axios = require("axios");
+
+const options = {
+  method: 'POST',
+  url: 'https://books17.p.rapidapi.com/authors/8418015/works',
+  headers: {
+    'content-type': 'application/json',
+    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+    'X-RapidAPI-Host': 'books17.p.rapidapi.com'
+  },
+  data: '{"cursor":1}'
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
+  } catch(err) {
+    res.status(400).json(err.message)
+  }
+})
+
+
+// Lista 
+let products = [
+  {
+    id: nanoid(),
+    bookName: "The Great Gatsby",
+    author: "(F. Scott Fitzgerald)", 
+    price: 200
+  },
+  {
+    id: nanoid(),
+    bookName: "Narnia",
+    author: "(C. S. Lewis)"
+  },
+  {
+    id: nanoid(),
+    bookName: "The Habbit",
+    author: "(J. R. R. Tolkien)"
+  }
+]
+
+
+app.get("/products", (req, res) => {
+  
+  try {
+    res.json(products)
+  } catch (err) {
+    res.status(500).json(err.message)
+  }
+})
+
+//  POST
+app.post("/products", (req, res) => {
+  
+  try {
+    if (!req.body || (!req.body.bookName || !req.body.author)) {
+      throw new Error("Data was not provided correctly!")
+    }
+
+    /*const productExist = products.find(product => product.bookName == req.body.bookName)
+
+    if (productExist) {
+      throw new Error("Product already exist")
+    }*/
+
+/*  let newProduct = req.body
+    newProduct.id = nanoid()
+    products.push(newProduct)
+    res.json("New Product added!") */
+
+    products.push({...req.body, ...{id: nanoid()}})
+    res.json({status: "New product added!"}) 
+
+  } catch (err) {
+    res.status(400).json(err.message)
+  }
+
+})
+
+
+app.use((err, req, res, next) => {
+  console.log(err.status)
+  console.log(err.message)
+  res.status(500).json(err)
+})
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+})
+
+
+
